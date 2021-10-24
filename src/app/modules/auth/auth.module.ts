65bc3f9e3cfaeb7fad1a673886d39012fe0c95fc
Geovanny <geovany.afanador@gmail.com>
2021-10-23 23:27:52 -0500

    feat: se agregó los componentes a los modulos

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
